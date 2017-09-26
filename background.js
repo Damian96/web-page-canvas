410914b9c4d9f497dab02e97e2f3e746d9ec9296
Damian <damgiankakis@gmail.com>
2017-09-26 19:10:14 +0300

    Continues change to popup

--- background.js
+++ background.js
@@ -1,93 +1,31 @@
 /* globals chrome */
 
-var highlighterOpen = [];
-var screenshotInjeced = [];
+var canvasOpen = [];
+var popupObjects = [];
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-    if(highlighterOpen[tab.id] == null) {
-        var basePath = chrome.runtime.getURL('/');
-        chrome.tabs.executeScript(tab.id,
-        {
-            code: "var EXTENSIONPATH = '" + basePath + "';\n"
-        });
-        chrome.tabs.executeScript(tab.id,
-        {
-            file: '/content-scripts/js/init.min.js'
-        });
-        highlighterOpen[tab.id] = true;
-    } else if(highlighterOpen[tab.id]) {
-        closeOverlay();
-        highlighterOpen[tab.id] = undefined;
+    if(popupObjects[tab.id] != null) {
+        chrome.runtime.sendMessage({message: 'initMain', data: popupObjects[tab.id]});
+    } else {
+        chrome.runtime.sendMessage({message: 'initMain', data: false});
     }
+    // if(canvasOpen[tab.id] == null) {
+    //     canvasOpen[tab.id] = true;
+    // } else if(canvasOpen[tab.id]) {
+    //     canvasOpen[tab.id] = undefined;
+    // }
 });
-
-function closeOverlay(tabId) {
-    chrome.tabs.executeScript(tabId,
-    {
-        code: "highlightorObject.remove();\n"
-    });
-}
 
 chrome.tabs.onUpdated.addListener(handleUpdated);
 
 function handleUpdated(tabId) {
-    screenshotInjeced[tabId] = undefined;
-    if(typeof highlighterOpen[tabId] === 'boolean') {
-        highlighterOpen[tabId] = undefined;
+    if(canvasOpen[tabId]) {
+        canvasOpen[tabId] = undefined;
     }
 }
 
 chrome.runtime.onMessage.addListener(function(request, sender) {
-    if(sender.tab) {
-        if((request.handleOverlay == false)) {
-            highlighterOpen[sender.tab.id] = undefined;
-            screenshotInjeced[sender.tab.id] = undefined;
-        }
-        if(request.takeScreenshot && (typeof request.canvasSrc === 'string')) {
-            closeOverlay(sender.tab.id);
-            var callFn = "takeHighlighterScreenshot('" + request.canvasSrc + "');";
-            if(screenshotInjeced[sender.tab.id] == null) {
-                chrome.tabs.executeScript(sender.tab.id,
-                {
-                    file: '/content-scripts/js/html2canvas.min.js'
-                });
-                var code = "function takeHighlighterScreenshot(src) {";
-                code += "if(confirm('Please wait 2 seconds for the page to be rendered.')) {";
-                code += "var screenshot = document.createElement('img');\n";
-                code += "screenshot.id = 'highlighter-screenshot';\n";
-                code += "screenshot.src = src;\n";
-                code += "screenshot.style.position = 'absolute';\n";
-                code += "screenshot.style.zIndex = 2000000;\n";
-                code += "var dimensions = getPageDimensions();\n";
-                code += "screenshot.width = dimensions.width;\n";
-                code += "screenshot.height = dimensions.height;\n";
-                code += "document.body.appendChild(screenshot);\n";
-                code += "html2canvas(document.body, {\n";
-                code += "onrendered: function(canvas) {\n";
-                code += "document.getElementById('highlighter-screenshot').remove();\n";
-                code += "var link = document.createElement('a');\n";
-                code += "link.className = 'highlighter-download';\n";
-                code += "link.href = canvas.toDataURL();\n";
-                code += "link.style.display = 'none';\n";
-                code += "var date = new Date()\n;";
-                code += "link.download = location.hostname + '_' + date.getTime() + '.png';\n";
-                code += "document.body.appendChild(link);\n";
-                code += "link.click();\n";
-                code += "}});\n}\n}";
-                setTimeout(function() {
-                    chrome.tabs.executeScript(sender.tab.id,
-                        {
-                            code: code + callFn
-                        });
-                }, 1000);
-                screenshotInjeced[sender.tab.id] = true;
-            } else {
-                chrome.tabs.executeScript(sender.tab.id,
-                {
-                    code: callFn
-                });
-            }
-            highlighterOpen[sender.tab.id] = undefined;
-        }
+    if((request.saveObject != null) && request.saveObject) {
+        popupObjects[sender.tab.id] = request.data;
     }
 });
--- popup/js/beautified/highlight-popup.js
+++ popup/js/beautified/highlight-popup.js
@@ -5,275 +5,146 @@
  * Used for creating the Highlighting Mode layout.
  * Implements the drawing function code. 
  */
-class Highlightor {
+class Main {
     constructor() {
-        this.activeIcon = {
-            id: 'highlighter'
+        this.activeTool = {
+            id: 'paint-brush'
         };
-        this.canvas = {
-            clickX: [],
-            clickY: [],
-            clickDrag: [],
-            clickColor: [],
-            isDrawing: false,
-            brushColor: '#0000FF',
-            markerColor: '#FFFF00',
-            highlightColor: '#ff0000',
-            lineWidth: 5,
-            drawEnabled: false,
-            element: null,
-            context: null
+        this.toolInfo = {
+            paintBrush: {
+                color: '#FFFF00',
+                defaultColor: '#FFFF00',
+                size: 5,
+                opacity: 1
+            },
+            eraser: {
+                size: 5
+            }
         };
-        this.htmlInserted = false;
-        this.downloads = 0;
     }
 
     init() {
         this.attachHandlers();
-        this.initCanvas();
-        chrome.tabs.getCurrent(function(tab) {
-            document.querySelector('#overlay-message').style.top = tab.height / 2 + 'px';
-        });
+    }
+
+    reload() {
+        this.attachHandlers();
+        this.reloadValues();
     }
 
     attachHandlers() {
-        var closeIcon = document.querySelector("#close-overlay.highlighter"),
-            confirmBtn = document.querySelector("#confirm-message.highlighter");
-        closeIcon.addEventListener("click", function() {
-            closeIcon.parentElement.style.display = "none";
-            this.canvas.drawEnabled = true;
-        }.bind(this, closeIcon));
-        confirmBtn.addEventListener('click', function() {
-            confirmBtn.parentNode.parentElement.style.display = "none";
-            this.canvas.drawEnabled = true;
-        }.bind(this, confirmBtn));
-        Array.from(document.querySelectorAll("#toolbar.highlighter .tool-container")).forEach(function(element) {
-            element.addEventListener('click', this.iconClickHandler.bind(this, element));
-            element.addEventListener('mouseenter', this.optionsPopupHandler.bind(this, element));
-            element.addEventListener('mouseleave', this.optionsPopupHandler.bind(this, element));
-        }.bind(this));
-        document.querySelector('#toolbar.highlighter #save-page').addEventListener('click', function() {
-            chrome.runtime.sendMessage({
-                takeScreenshot: true,
-                canvasSrc: document.querySelector('canvas').toDataURL()
-            });
-        }.bind(this));
-        document.querySelector('#toolbar.highlighter #toolbar-alignment').addEventListener('click', function() {
-            var toolbar = document.querySelector('#toolbar.highlighter');
-            if(toolbar.classList.contains('aligned-top')) {
-                toolbar.classList.remove('aligned-top');
-                toolbar.classList.add('aligned-right');
-            } else if(toolbar.classList.contains('aligned-right')) {
-                toolbar.classList.remove('aligned-right');
-                toolbar.classList.add('aligned-top');                
-            }
-        });
-        document.querySelector('#toolbar.highlighter #close-toolbar').addEventListener('click', function() {
-            this.closeOverlay();
-        }.bind(this));
-        Array.from(document.querySelectorAll('.highlighter.popup span.color')).forEach(function(element) {
-            element.addEventListener('mousedown', this.toolColorClickHandler.bind(this, element));
-        }.bind(this));
+        Array.from(document.querySelectorAll('.tab-title.tool')).forEach(function(element) {
+            element.addEventListener('click', this.toolClickHandler.bind(this, element));
+        }, this);
+        Array.from(document.querySelectorAll('.tab-content.active .color'))
+        .forEach(function(element) {
+            element.addEventListener('click', this.colorClickHandler.bind(this, element));
+        }, this);
+        Array.from(document.querySelectorAll('.tab-content.active input.size-range, .tab-content.active input.opacity-range'))
+        .forEach(function(element) {
+            element.addEventListener('change', this.rangeHandler.bind(this, element));            
+        }, this);
     }
 
-    closeOverlay() {
-        chrome.runtime.sendMessage({handleOverlay: false});
-    }
-
-    keepToolbarFixed(scrollY) {
-        var toolbar = document.querySelector('#toolbar');
-        if(scrollY > 0) {
-            toolbar.style.position.top = scrollY + 'px';
-        } else {
-            toolbar.style.position.top = '0px';
+    reloadValues() {
+        var dataSelector = "[data-tool-id='" + this.activeTool.id + "']";
+        var toolElement = document.querySelector(".tab-title" + dataSelector);
+        this.toolClickHandler(toolElement);
+        if(this.activeTool.id === 'paint-brush') {
+            var colorElement = document.querySelector(".tab-content" + dataSelector + " .color[data-color-code='" + this.toolInfo.paintBrush.colorCode)
+            var sizeElement = document.querySelector(".tab-content" + dataSelector + " .size-range");
+            var opacityElement = document.querySelector(".tab-content" + dataSelector + " .opacity-range");
+            this.colorClickHandler(colorElement);
+            this.rangeHandler(sizeElement);
+            this.rangeHandler(opacityElement);
+        } else if(this.activeTool.id === 'eraser') {
+            // var sizeElement = document.querySelector(".tab-content" + dataSelector + " .size-range");
         }
     }
 
-    getCurrentDate() {
-        var today = new Date();
-        var days = today.getDate();
-        var months = today.getMonth() + 1;
-        var years = today.getFullYear();
-        var hours  = today.getHours();
-        var minutes = today.getMinutes();
-        var seconds = today.getSeconds();
-
-        days = this.addZero(days);
-        months = this.addZero(months);
-        years = this.addZero(years);
-        hours = this.addZero(hours);
-        minutes = this.addZero(minutes);
-        seconds = this.addZero(seconds);
-
-        return days + '-' + months + '-' + years + '_' + hours + '-' + minutes + '-' + seconds;
-    }
-
-    addZero(num) {
-        if(num < 10) {
-            return '0' + num;
-        }
-        return num;
-    }
-
-    initCanvas() {
-        // assign proper variables
-        this.canvas.element = document.querySelector('#canvas.highlighter');
-        this.canvas.element.width = window.innerWidth;
-        this.canvas.element.height = document.documentElement.offsetHeight;
-        this.canvas.context = this.canvas.element.getContext('2d');
-        this.canvas.context.fillCircle = function(x, y, radius, fillColor) {
-            this.fillStyle = fillColor;
-            this.beginPath();
-            this.moveTo(x, y);
-            this.arc(x, y, radius, 0, Math.PI * 2, false);
-            this.fill();
-        };
-        this.canvas.context.clearAll = function() {
-            this.canvas.context.clearRect(0, 0, this.canvas.element.width, this.canvas.element.height);
-        }.bind(this);
-    
-        // bind mouse events
-        this.canvas.element.onmousemove = function(e) {
-            if(this.canvas.drawEnabled && this.canvas.isDrawing && (this.activeIcon.id === 'paint-brush')) {
-                var x = e.pageX - this.canvas.element.offsetLeft;
-                var y = e.pageY - this.canvas.element.offsetTop;
-                this.addClick(x, y, true);
-                this.draw();
-            }
-        }.bind(this);
-        this.canvas.element.onmousedown = function(e) {
-            if(this.activeIcon.id === 'paint-brush') {
-                var x = e.pageX - this.canvas.element.offsetLeft;
-                var y = e.pageY - this.canvas.element.offsetTop;
-                this.canvas.isDrawing = true;
-                this.addClick(x, y, false);            
-                this.draw();
-            }
-        }.bind(this);
-        this.canvas.element.onmouseup = function() {
-            this.canvas.isDrawing = false;
-        }.bind(this);
-        this.canvas.element.onmouseleave = function() {
-            this.canvas.isDrawing = false;
-        }.bind(this);
-    }
-
-    addClick(x, y, dragging) {
-        this.canvas.clickX.push(x);
-        this.canvas.clickY.push(y);
-        this.canvas.clickDrag.push(dragging);
-        this.canvas.clickColor.push(this.canvas.brushColor);
-    }
-
-    draw() {
-        this.canvas.context.lineJoin = "round";
-        this.canvas.context.lineWidth = this.canvas.lineWidth;
-        for(var i=0; i < this.canvas.clickX.length; i++) {
-            this.canvas.context.beginPath();
-            if(this.canvas.clickDrag[i] && i){
-                this.canvas.context.moveTo(this.canvas.clickX[i - 1], this.canvas.clickY[i - 1]);
-            } else {
-                this.canvas.context.moveTo(this.canvas.clickX[i] - 1, this.canvas.clickY[i]);
-            }
-            this.canvas.context.lineTo(this.canvas.clickX[i], this.canvas.clickY[i]);
-            this.canvas.context.closePath();
-            this.canvas.context.strokeStyle = this.canvas.clickColor[i];
-            this.canvas.context.stroke();
-        }
-    }
-    
-    clearCanvas() {
-        this.canvas.clickX = [];
-        this.canvas.clickY = [];
-        this.canvas.clickDrag = [];
-        this.canvas.context.clearRect(0, 0, this.canvas.element.width, this.canvas.element.height);
-    }
-
-    iconClickHandler(element) {
-        'use strict';
-        if(element.title === 'Clear All') {
-            this.clearCanvas();
-            this.disableAllIcons();
-            return;
-        }
-        var isActive = element.classList.contains('active');
-        this.disableAllIcons();
-        if(!isActive) {
+    toolClickHandler(element) {
+        if(!element.classList.contains('active')) {
+            var id = element.dataset.toolId;
+            this.disableAllTools();
             element.classList.add('active');
-            var icon = element.firstElementChild;
-            this.activeIcon.id = icon.id;
-            this.changeToolColor(icon.id);
-        } else {
-            element.style.borderBottomColor = '';
-            element.style.borderLeftColor = '';
+            document.querySelector(".tab-content[data-tool-id='" + id + "']").classList.add('active');
+            this.activeTool.id = id;
         }
     }
 
-    changeToolColor(iconId) {
-        'use strict';
-        var element = document.querySelector('#toolbar.highlighter i#' + iconId).parentElement;
-        switch(iconId) {
-            case 'paint-brush':
-                element.style.borderBottomColor = this.canvas.brushColor;
-                element.style.borderLeftColor = this.canvas.brushColor;
-                break;
+    colorClickHandler(element) {
+        if(!element.classList.contains('active')) {
+            var tabContent = element.parentElement.parentElement,
+                toolId = tabContent.dataset.toolId,
+                colorName = element.title.toLowerCase();
+            this.disableAllColors(toolId);
+            element.classList.add('active');
+            if(toolId === 'paint-brush') {
+                this.toolInfo.paintBrush.color = element.dataset.colorCode;
+            }
+            tabContent.dataset.toolColor = colorName;
+            document.querySelector(".tab-title[data-tool-id='" + toolId + "']").dataset.toolColor = colorName;
         }
     }
 
-    toolColorClickHandler(element) {
-        'use strict';
-        var toolId = element.parentElement.id;
-        if((toolId == null) || (toolId === 'eraser')) {
-            return;
-        }
-        switch(toolId) {
-            case 'paint-brush':
-                this.canvas.brushColor = element.id;
-                break;
-            case 'highlighter':
-                this.canvas.markerColor = element.id;
-                break;
-        }
-        this.changeToolColor(toolId);
-    }
-
-    optionsPopupHandler(element, event) {
-        'use strict';
-        var id = element.firstElementChild.id;
-        var popup = document.querySelector('#' + id + '.popup.highlighter');
-        if((popup == null) || !element.classList.contains('active')) {
-            return;
-        }
-        if((event.type === 'mouseenter') && (popup.classList.contains('hidden'))) {
-            var rect = element.getBoundingClientRect();
-            var x = rect.x;
-            var y = rect.y + element.offsetHeight;
-            popup.classList.remove('hidden');
-            popup.classList.add('visible');
-            popup.style.left = x + 'px';
-            popup.style.top = y + 'px';
-        } else if((event.type === 'mouseleave') && (popup.classList.contains('visible'))) {
-            if((event.toElement.id === id) && event.toElement.classList.contains('popup')) {
-                popup.addEventListener('mouseleave', this.hidePopup.bind(this, popup));
-                popup.addEventListener('mousedown', this.hidePopup.bind(this, popup));
-            } else {
-                this.hidePopup(popup);
+    rangeHandler(element) {
+        var toolId = element.dataset.toolId,
+            value = null,
+            type = null;
+        if(element.classList.contains('size-range')) {
+            value = parseFloat(element.value);
+            type = 'size';            
+            element.nextElementSibling.innerHTML = value + 'px';
+        } else if(element.classList.contains('opacity-range')) {
+            type = 'opacity';
+            value = 1 - parseFloat(element.value);
+            if((value > 0) && (value < 1)) {
+                element.nextElementSibling.innerHTML = element.value.charAt(2) + '0%';
+            } else if(value == 0) {
+                element.nextElementSibling.innerHTML = '100%';
+            } else if(value == 1) {
+                element.nextElementSibling.innerHTML = '0%';
             }
         }
+        if((toolId === 'paint-brush') && (value != null)) {
+            if(type === 'size') {
+                this.toolInfo.paintBrush.size = value;
+            } else if(type === 'opacity') {
+                this.toolInfo.paintBrush.opacity = value;
+            }
+        } else if((toolId === 'eraser') && (type === 'size') && (value != null)) {
+            this.toolInfo.eraser.size = value;
+        }
     }
 
-    hidePopup(element) {
-        element.classList.remove('visible');
-        element.classList.add('hidden');
-    }
-
-    disableAllIcons() {
-        this.activeIcon.id = null;
-        Array.from(document.querySelectorAll('#toolbar.highlighter .tool-container.active')).forEach(function(element) {
+    disableAllTools() {
+        Array.from(document.querySelectorAll('.tab-title.active, .tab-content.active'))
+        .forEach(function(element) {
             element.classList.remove('active');
         });
     }
+
+    disableAllColors(toolId) {
+        var contentSelector = ".tab-content[data-tool-id='" + toolId + "']";
+        Array.from(document.querySelectorAll(contentSelector + " .color.active"))
+        .forEach(function(element) {
+            element.classList.remove('active');
+        }, this);
+    }
 }
 
-var object = new Highlightor();
-object.init();
+window.onbeforeunload = function() {
+    chrome.runtime.sendMessage({saveObject: true, data: JSON.stringify(object)});
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender) {
+    if((request.message === 'initMain')) {
+        if(!data) {
+            var object = new Main();
+            object.init();
+        } else {
+            var object = JSON.parse(request.data);
+            object.reload();
+        }
+    }
+});
